8aa980ee019db6a9ebf35bc30591089a342e34cf
Julie Nisbet <jmnisbet@gmail.com>
2022-11-01 12:09:30 -0700

    demo classes

--- classes.js
+++ classes.js
@@ -1,11 +1,33 @@
-class Vehicle {}
+class Vehicle {
+  constructor(owner, wheels, brand) {
+    this.owner = owner;
+    this.wheels = wheels;
+    this.brand = brand;
+  }
+}
 
-class Motorcycle extends Vehicle {}
+class Motorcycle extends Vehicle {
+  constructor(owner, brand) {
+    super(owner, 2, brand);
+  }
+}
 
-class Honda extends Motorcycle {}
+class Honda extends Motorcycle {
+  constructor(owner) {
+    super(owner, 'Honda');
+  }
+}
 
-class Car {}
+class Car extends Vehicle {
+  constructor(owner, brand) {
+    super(owner, 4, brand);
+  }
+}
 
-class Tesla {}
+class Tesla extends Car {
+  constructor(owner) {
+    super(owner, 'Tesla');
+  }
+}
 
 module.exports = { Vehicle, Motorcycle, Honda, Car, Tesla };
--- example.js
+++ example.js
@@ -1,7 +1,15 @@
 // const benny = {
-//   name: "Benny",
+//   name: 'Benny',
 //   age: 7,
-//   speak: function () {
+//   speak() {
+//     console.log(`${this.name} says bark bark!`);
+//   },
+// };
+
+// const finley = {
+//   name: 'Finley',
+//   age: 7,
+//   speak() {
 //     console.log(`${this.name} says bark bark!`);
 //   },
 // };
@@ -20,21 +28,43 @@
 // Animal.prototype.describeAnimal = function () {
 //   console.log(`name: ${this.name}, age: ${this.age}`);
 // };
-// const benny = new Animal("Benny", 7);
+
+// const benny = new Animal('Benny', 7);
+// // benny.describeAnimal = function () {
+// //   console.log(`name: ${this.name}, age: ${this.age}`);
+// // };
+// const finley = new Animal('Finley', 4);
 // benny.speak();
 // benny.describeAnimal();
-// console.log(benny.__proto__);
+// console.log(benny.age);
+// finley.describeAnimal();
+// benny.speak();
+// benny.describeAnimal();
 
 class Animal {
-  constructor(name, age) {
+  // favoriteToy = 'stuffy';
+
+  constructor(name, age, favoriteToy = 'stuffy') {
     this.name = name;
     this.age = age;
+    this.favoriteToy = favoriteToy;
   }
 
   speak() {
     console.log(`${this.name} says bark bark!`);
   }
+
+  describeAnimal() {
+    console.log(`name: ${this.name}, age: ${this.age}`);
+  }
 }
 
 const benny = new Animal('Benny', 7);
+console.log(benny);
+const finley = new Animal('Finley', 4, 'ball');
 benny.speak();
+benny.describeAnimal();
+console.log(finley.favoriteToy);
+
+finley.favoriteToy = 'kong';
+console.log(finley.favoriteToy);
